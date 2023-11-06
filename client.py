400c4cd71fc6a02e2cb035287f418fcd146828a7
HBzhainan-wzw <zhiwen_wu@ucsb.edu>
2023-11-06 10:27:36 -0800

    1

--- client.py
+++ client.py
@@ -1,26 +1,33 @@
 import cv2
+import socket
+import numpy as np
 
-# Replace the below URL with your stream URL
-stream_url = 'udp://@:1234'  # This is the address where the stream is received.
+# Set up the socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Bind to any incoming data on this port
+sock.bind(('0.0.0.0', 10001))
 
-# Use OpenCV to capture the video stream
-cap = cv2.VideoCapture(stream_url)
+# Assuming a maximum packet size of 65507 bytes (though it might be less)
+MAX_DGRAM_SIZE = 65507
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+try:
+    # Loop to continuously receive data
+    while True:
+        # Receive data
+        packet_data, addr = sock.recvfrom(MAX_DGRAM_SIZE)
+        
+        # Here, we directly decode each packet's data. In a real application, you would need
+        # to handle packet reordering, and possibly reconstruct the frame from several packets
+        # if your frame size exceeds the packet size.
+        frame = np.frombuffer(packet_data, dtype=np.uint8)
+        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
-    if not ret:
-        print("Failed to retrieve frame. Exiting...")
-        break
-
-    # Display the resulting frame
-    cv2.imshow('Video Stream', frame)
-
-    # Press 'q' on the keyboard to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything is done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        # Check if frame is valid
+        if frame is not None:
+            cv2.imshow('Video Stream', frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+finally:
+    sock.close()
+    cv2.destroyAllWindows()
