e9f7ffd1efb249c1d6599f0402f2568d65c35100
HBzhainan-wzw <zhiwen_wu@ucsb.edu>
2023-11-06 11:02:57 -0800

    1

--- server.py
+++ server.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-
 import socket
 import time
 
+import picamera
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FileOutput
@@ -12,19 +11,9 @@ video_config = picam2.create_video_configuration({"size": (1280, 720)})
 picam2.configure(video_config)
 encoder = H264Encoder(1000000)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("0.0.0.0", 10001))
-    sock.listen()
-
-    picam2.encoders = encoder
-
-    conn, addr = sock.accept()
-    stream = conn.makefile("wb")
-    encoder.output = FileOutput(stream)
-    picam2.start_encoder(encoder)
-    picam2.start()
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.connect(("REMOTEIP", 10001))
+    stream = sock.makefile("wb")
+    picam2.start_recording(encoder, FileOutput(stream))
     time.sleep(20)
-    picam2.stop()
-    picam2.stop_encoder()
-    conn.close()
+    picam2.stop_recording()
